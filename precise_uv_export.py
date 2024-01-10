dacc44b569fe44054e6fb46e3e71c8b28603ba78
majikdev <majikhq@gmail.com>
2024-01-10 15:14:54 +0200

    Removed nested functions

--- precise_uv_export.py
+++ precise_uv_export.py
@@ -77,116 +77,33 @@ class ExportLayout(bpy.types.Operator):
         return {'FINISHED'}
 
     def export_uv_layout(self, path, triangles):
-        def draw_triangle(x1, y1, x2, y2, x3, y3, island):
-            for x in range(x_min, x_max):
-                for y in range(y_min, y_max):
-                    dist_a = (x - x2) * (y1 - y2) - (x1 - x2) * (y - y2)
-                    dist_b = (x - x3) * (y2 - y3) - (x2 - x3) * (y - y3)
-                    dist_c = (x - x1) * (y3 - y1) - (x3 - x1) * (y - y1)
+        self.width, self.height = self.size
 
-                    negative = dist_a < 0 or dist_b < 0 or dist_c < 0
-                    positive = dist_a > 0 or dist_b > 0 or dist_c > 0
-
-                    if not (negative and positive):
-                        indices[y * width + x] = island
-
-        def draw_line(x1, y1, x2, y2, island):
-            length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-            x_dir, y_dir = (x2 - x1) / length, (y2 - y1) / length
-            x, y, dist = int(x1), int(y1), 0
-
-            x_delta = 1e6 if x_dir == 0 else abs(1 / x_dir)
-            y_delta = 1e6 if y_dir == 0 else abs(1 / y_dir)
-
-            if x_dir < 0:
-                x_step = -1
-                x_dist = (x1 - x) * x_delta
-            else:
-                x_step = 1
-                x_dist = (x - x1 + 1) * x_delta
-
-            if y_dir < 0:
-                y_step = -1
-                y_dist = (y1 - y) * y_delta
-            else:
-                y_step = 1
-                y_dist = (y - y1 + 1) * y_delta
-
-            while dist < length:
-                if x_min <= x < x_max and y_min <= y < y_max:
-                    indices[y * width + x] = island
-
-                if x_dist < y_dist:
-                    x_dist += x_delta
-                    x += x_step
-                    dist = x_dist - x_delta
-                else:
-                    y_dist += y_delta
-                    y += y_step
-                    dist = y_dist - y_delta
-
-        def get_colour(x, y):
-            index = indices[y * width + x]
-
-            if index != -1:
-                value = 1
-
-                if self.shade_islands:
-                    value -= (index % 6) * 0.1
-
-                if self.grid_overlay and (x + y) % 2 == 1:
-                    value -= 0.04
-
-                return value, value, value, 1
-
-            if self.outline_islands:
-                has_l = x > 0
-                has_r = x < width - 1
-                has_t = y > 0
-                has_b = y < height - 1
-
-                neighbours  = has_l and indices[y * width + (x - 1)] >= 0
-                neighbours += has_r and indices[y * width + (x + 1)] >= 0
-                neighbours += has_t and indices[(y - 1) * width + x] >= 0
-                neighbours += has_b and indices[(y + 1) * width + x] >= 0
-                neighbours += has_l and has_t and indices[(y - 1) * width + (x - 1)] >= 0
-                neighbours += has_r and has_t and indices[(y - 1) * width + (x + 1)] >= 0
-                neighbours += has_l and has_b and indices[(y + 1) * width + (x - 1)] >= 0
-                neighbours += has_r and has_b and indices[(y + 1) * width + (x + 1)] >= 0
-
-                if neighbours > 0:
-                    return 0, 0, 0, 1
-
-            return 0, 0, 0, 0
-
-        # Draw triangles to a buffer.
-
-        width, height = self.size
-        indices = [-1] * (width * height)
+        self.indices = [-1] * (self.width * self.height)
 
         for triangle in triangles:
             island_index = triangle.pop()
-            v1, v2, v3 = [(x * width, y * height) for x, y in triangle]
+            v1, v2, v3 = [(x * self.width, y * self.height) for x, y in triangle]
 
-            x_min, x_max = max(min(v1[0], v2[0], v3[0]), 0), min(max(v1[0], v2[0], v3[0]), width)
-            y_min, y_max = max(min(v1[1], v2[1], v3[1]), 0), min(max(v1[1], v2[1], v3[1]), height)
+            x_min, x_max = max(min(v1[0], v2[0], v3[0]), 0), min(max(v1[0], v2[0], v3[0]), self.width)
+            y_min, y_max = max(min(v1[1], v2[1], v3[1]), 0), min(max(v1[1], v2[1], v3[1]), self.height)
 
             x_min = ceil(x_min) if isclose(x_min, ceil(x_min), rel_tol=1e-4) else int(x_min)
             y_min = ceil(y_min) if isclose(y_min, ceil(y_min), rel_tol=1e-4) else int(y_min)
             x_max = int(x_max) if isclose(x_max, int(x_max), rel_tol=1e-4) else ceil(x_max)
             y_max = int(y_max) if isclose(y_max, int(y_max), rel_tol=1e-4) else ceil(y_max)
 
-            draw_triangle(*v1, *v2, *v3, island_index)
-            draw_line(*v1, *v2, island_index)
-            draw_line(*v2, *v3, island_index)
-            draw_line(*v3, *v1, island_index)
+            self.draw_triangle(*v1, *v2, *v3, x_min, x_max, y_min, y_max, island_index)
+            self.draw_line(*v1, *v2, x_min, x_max, y_min, y_max, island_index)
+            self.draw_line(*v2, *v3, x_min, x_max, y_min, y_max, island_index)
+            self.draw_line(*v3, *v1, x_min, x_max, y_min, y_max, island_index)
 
         # Create and save the image.
 
-        pixels = [i for y in range(height) for x in range(width) for i in get_colour(x, y)]
+        pixels = [i for y in range(self.height) for x in range(self.width) for i in self.get_colour(x, y)]
 
         try:
-            image = bpy.data.images.new('temp', width, height, alpha=True)
+            image = bpy.data.images.new('temp', self.width, self.height, alpha=True)
             image.filepath = path
             image.pixels = pixels
             image.save()
@@ -194,6 +111,88 @@ class ExportLayout(bpy.types.Operator):
             bpy.data.images.remove(image)
         except:
             pass
+
+    def draw_triangle(self, x1, y1, x2, y2, x3, y3, x_min, x_max, y_min, y_max, island):
+        for x in range(x_min, x_max):
+            for y in range(y_min, y_max):
+                dist_a = (x - x2) * (y1 - y2) - (x1 - x2) * (y - y2)
+                dist_b = (x - x3) * (y2 - y3) - (x2 - x3) * (y - y3)
+                dist_c = (x - x1) * (y3 - y1) - (x3 - x1) * (y - y1)
+
+                negative = dist_a < 0 or dist_b < 0 or dist_c < 0
+                positive = dist_a > 0 or dist_b > 0 or dist_c > 0
+
+                if not (negative and positive):
+                    self.indices[y * self.width + x] = island
+
+    def draw_line(self, x1, y1, x2, y2, x_min, x_max, y_min, y_max, island):
+        length = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        x_dir, y_dir = (x2 - x1) / length, (y2 - y1) / length
+        x, y, dist = int(x1), int(y1), 0
+
+        x_delta = 1e6 if x_dir == 0 else abs(1 / x_dir)
+        y_delta = 1e6 if y_dir == 0 else abs(1 / y_dir)
+
+        if x_dir < 0:
+            x_step = -1
+            x_dist = (x1 - x) * x_delta
+        else:
+            x_step = 1
+            x_dist = (x - x1 + 1) * x_delta
+
+        if y_dir < 0:
+            y_step = -1
+            y_dist = (y1 - y) * y_delta
+        else:
+            y_step = 1
+            y_dist = (y - y1 + 1) * y_delta
+
+        while dist < length:
+            if x_min <= x < x_max and y_min <= y < y_max:
+                self.indices[y * self.width + x] = island
+
+            if x_dist < y_dist:
+                x_dist += x_delta
+                x += x_step
+                dist = x_dist - x_delta
+            else:
+                y_dist += y_delta
+                y += y_step
+                dist = y_dist - y_delta
+
+    def get_colour(self, x, y):
+        index = self.indices[y * self.width + x]
+
+        if index != -1:
+            value = 1
+
+            if self.shade_islands:
+                value -= (index % 6) * 0.1
+
+            if self.grid_overlay and (x + y) % 2 == 1:
+                value -= 0.04
+
+            return value, value, value, 1
+
+        if self.outline_islands:
+            has_l = x > 0
+            has_r = x < self.width - 1
+            has_t = y > 0
+            has_b = y < self.height - 1
+
+            neighbours  = has_l and self.indices[y * self.width + (x - 1)] >= 0
+            neighbours += has_r and self.indices[y * self.width + (x + 1)] >= 0
+            neighbours += has_t and self.indices[(y - 1) * self.width + x] >= 0
+            neighbours += has_b and self.indices[(y + 1) * self.width + x] >= 0
+            neighbours += has_l and has_t and self.indices[(y - 1) * self.width + (x - 1)] >= 0
+            neighbours += has_r and has_t and self.indices[(y - 1) * self.width + (x + 1)] >= 0
+            neighbours += has_l and has_b and self.indices[(y + 1) * self.width + (x - 1)] >= 0
+            neighbours += has_r and has_b and self.indices[(y + 1) * self.width + (x + 1)] >= 0
+
+            if neighbours > 0:
+                return 0, 0, 0, 1
+
+        return 0, 0, 0, 0
 
     @staticmethod
     def get_image_size(context, default):
